45bf9bb7348dafdb8d70fae65281331cc6fc62f3
tenzinsherap <60201749+tenzinsherap@users.noreply.github.com>
2020-04-19 02:48:51 -0400

    Add files via upload

---
+++ labs/8/Cylinder.java
@@ -0,0 +1,15 @@
+package labs;
+
+public class Cylinder extends Shape{
+	   double radius ;
+	   double height ;
+	   double area() {
+	       return Math.PI * radius * radius * height ;
+	   }
+	  
+	   public String toString() {
+	       return "Cylinder [shapeName=" + shapeName + ", height=" + height + ", radius=" + radius + ", Surface area=" + area()
+	               + "]";
+	   }
+	  
+	}
